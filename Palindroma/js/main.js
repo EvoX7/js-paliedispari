bea43f63afd12e73f7fe8d4374114436b789ba07
EvoX7 <creativesoultouch@gmail.com>
2022-06-22 15:52:47 +0200

    Palindrome Words finished

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,9 +1,25 @@
-
 // ! Asking user for an input word 
-const userInput = ("Please enter a word");
+
+const userInput = prompt("Please enter a word");
 
 
-// ? Check if a word is palindrome 
-function checkPal(){
+// ? Checking if a word is palindrome 
+// ? Naming a function
+function checkPal(userInput) {
+    const userInputLength = userInput.length;
 
+    // ? Iterate through half of the string, because going through the whole string would compare every character twice
+    for (let index = 0; index < userInputLength / 2; index++) {
+
+        //    ? Comparing each character in start position [index] with each character in end position [index]
+        if (userInput[index] !== userInput[userInputLength - 1 - index]) {
+            return "The word it's NOT palindrome";
+        }
+    }
+    return "The word it's [Palindrome]";
+    
 }
+
+// * Defining output 
+const functionOutput = checkPal(userInput);
+console.warn(functionOutput);
