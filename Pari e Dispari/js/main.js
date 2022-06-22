ee37e9a009bfed8e8385adf2e0c30daff346eb3c
EvoX7 <creativesoultouch@gmail.com>
2022-06-22 19:32:58 +0200

    Even/Odd update 3

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,34 +1,38 @@
 // ! Asking input number from User
+const userChoice = prompt("Please choose Even/Odd");
 const userNumber = parseInt(prompt("Please enter a number from 1 to 5"));
+
+
+console.log("User choice is", userChoice);
 console.log("User number is", userNumber);
 document.getElementById("user-number").innerHTML = "User number is" + " " + userNumber;
+
+
 
 // ? Random number for the Computer
 function randomNumberC(minNumber, maxNumber) {
   return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
 }
 
+
 // ? Random number output
 const computerGen = randomNumberC(1, 5);
 console.log("Computer number is", computerGen);
 document.getElementById("computer-number").innerHTML = "Computer number is" + " " + computerGen;
 
+
 // ! Numbers  Sum
-const NumberSum = userNumber + computerGen;
+const numberSum = userNumber + computerGen;
 
 // * Checking Even/Odd
-function isEven(userNumber, computerGen) {
-  if (userNumber, computerGen % 2 === 0) return true;
+function isEven(numberSum) {
+  if (numberSum % 2 === 0) return true;
   else return false;
 }
 
-
-
-
-
 // ! Showing Even/Odd result
-const EvenOddResult = isEven(NumberSum);
+const EvenOddResult = isEven(numberSum);
 console.log(EvenOddResult);
-document.getElementById("even-odd").innerHTML = "Sum is:" + " " + NumberSum + " " + EvenOddResult;
+document.getElementById("even-odd").innerHTML = "Sum is:" + " " + numberSum;
 
 
